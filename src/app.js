601e125a23399f63e45561c15850c5f13349db5c
Muhammad Waqas <muhammadwaqas538039@gmail.com>
2025-10-14 15:07:59 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://waqas-mern-auth-frontend-seven-indol.vercel.app/", // your frontend domain
+    origin: "https://waqas-mern-auth-frontend-seven-indol.vercel.app", // your frontend domain
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
