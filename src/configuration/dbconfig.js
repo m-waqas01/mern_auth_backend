89be4367505697af271162d81ab836f4f7ab00ba
Muhammad Waqas <muhammadwaqas538039@gmail.com>
2025-10-21 13:44:32 +0000

    first commit

--- src/configuration/dbconfig.js
+++ src/configuration/dbconfig.js
@@ -4,8 +4,6 @@ const connectDB = async () => {
   try {
     console.log("🔍 Connecting to MongoDB...");
     await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 20000, // 20 seconds
-      family: 4, // force IPv4 (important for Vercel)
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
